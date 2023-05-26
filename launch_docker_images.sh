1ec89467d10237a2ca32cab581fb62a4852b90f3
Nanfack Steve <nanfacksteve7@gmail.com>
2023-05-26 10:42:28 +0100

    Refractoring - (change STEP 5 position)

--- launch_docker_images.sh
+++ launch_docker_images.sh
@@ -152,28 +152,28 @@ if $digit_is_running; then
 
     digit_is_ok=true
     errors_task=("Copying Digt Web App" "Extracting digit Web App" "Copying Certificates" "Changing Mode" "Deleting Index file")
-    
+
     echo -e "\nStart Configuring (Digit Web App)"
     echo -e "------------------------------------"
 
     # COPY OF WEB APP INTO DIGIT
     echo -ne "\nCopying $ylw( Digit Web App )$nc in container ...  "
-    docker cp "$web_app" "$DIGIT_CONTAINER":"$target_web_app_dir" 1> /dev/null 2>> "$log_file"
+    docker cp "$web_app" "$DIGIT_CONTAINER":"$target_web_app_dir" 1>/dev/null 2>>"$log_file"
     [[ $? -eq 0 ]] && echo -e "$grn[ ✔ ] done ! $nc\n" || { echo -e "$red[ ✗ ] failed ! $nc\n" && digit_is_ok=false && state=0; }
 
     # EXTRACT WEB APP
     echo -ne "Extract $ylw( Digit Web App )$nc in container ...  "
-    docker exec "$DIGIT_CONTAINER" unzip -o "$target_web_app_dir/$web_app" -d "$deploy_web_app_dir" 1> /dev/null 2>> "$log_file"
+    docker exec "$DIGIT_CONTAINER" unzip -o "$target_web_app_dir/$web_app" -d "$deploy_web_app_dir" 1>/dev/null 2>>"$log_file"
     [[ $? -eq 0 ]] && echo -e "$grn[ ✔ ] done ! $nc\n" || { echo -e "$red[ ✗ ] failed ! $nc\n" && digit_is_ok=false && state=1; }
 
     # COPY OF CERTIFICATES
     echo -ne "Copying $ylw( certificates )$nc  in container ...  "
-    docker cp "$certificates_dir" "$DIGIT_CONTAINER":"$certifates_web_app_dir" 1> /dev/null 2>> "$log_file"
+    docker cp "$certificates_dir" "$DIGIT_CONTAINER":"$certifates_web_app_dir" 1>/dev/null 2>>"$log_file"
     [[ $? -eq 0 ]] && echo -e "$grn[ ✔ ] done ! $nc\n" || { echo -e "$red[ ✗ ] failed ! $nc\n" && digit_is_ok=false && state=2; }
 
     # CHANGE ACCESS DIR
     echo -ne "Changing $ylw( certificates )$nc Access Mode  ...  "
-    docker exec "$DIGIT_CONTAINER" chmod 777 "$certificates_web_app_dir" 1> /dev/null 2>> "$log_file"
+    docker exec "$DIGIT_CONTAINER" chmod 777 "$certificates_web_app_dir" 1>/dev/null 2>>"$log_file"
     [[ $? -eq 0 ]] && echo -e "$grn[ ✔ ] done ! $nc\n" || { echo -e "$red[ ✗ ] failed ! $nc\n" && digit_is_ok=false && state=3; }
 
     # REMOVE HTML INDEX FILE FROM WEB_APP
@@ -191,6 +191,14 @@ if $digit_is_running; then
         echo -e "$grn#            DEPLOYMENT IS SUCCESSFUL            #"
         echo -e "$grn#"
         echo -e "$grn#@@@@@@@@@@@@@@@@@@@ $nc\n"
+
+        # +--------------------------------------------------------------+
+        # | STEP 5 : DELETE DOCKER STACK/COMPOSE And CERTIFICATES FOLDER |
+        # +--------------------------------------------------------------+
+
+        if [ -d "$docker_compose_dir" ]; then rm -r "$docker_compose_dir"; fi
+        if [ -d "$certificates_dir" ]; then rm -r "$certificates_dir"; fi
+
     else
         echo -e "\n$ylw[ Warning ] : Container $nc($DIGIT_CONTAINER)$ylw is running..$red but not complete configurate !\n"
         #echo -e "$red ✗ $nc -$red Somthing wrong on $ylw${errors_task[$state]} !$nc\n\n"
@@ -199,10 +207,3 @@ if $digit_is_running; then
 else
     echo -e "\n\n$red""[ Error ] :$nc $DIGIT_CONTAINER ""$red""is not running !$nc\n"
 fi
-
-# +--------------------------------------------------------------+
-# | STEP 5 : DELETE DOCKER STACK/COMPOSE And CERTIFICATES FOLDER |
-# +--------------------------------------------------------------+
-
-#if [ -d "$docker_compose_dir" ]; then rm -r "$docker_compose_dir"; fi
-#if [ -d "$certificates_dir" ]; then rm -r "$certificates_dir"; fi
